684f6fabec0ebd3727cee71549a45b35ac19c16d
Aleksandr Sysoev <aosysoev@narod.ru>
2022-07-28 17:22:01 +0300

    urls & apps

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # новые приложения
+    'bboard.apps.BBoardConfig'
 ]
 
 MIDDLEWARE = [
