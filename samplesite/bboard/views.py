e93b7146c0c9385544f8420adaec3338becb01c6
Aleksandr Sysoev <aosysoev@narod.ru>
2022-08-19 17:08:08 +0300

    before HTML templates

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from bboard.models import Bb
+
 def index(request):
-    return HttpResponse("Здесь будет список объявлений! Скоро! :-)")
+    s = 'Список объявлений \r\n\r\n'
+    for bb in Bb.objects.order_by('-published'):
+        s = s + bb.published.strftime("%m/%d/%Y, %H:%M:%S") + '\r\n' + bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    return HttpResponse(s, content_type='text/plain; charset=utf-8')
